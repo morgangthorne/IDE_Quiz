d469879530496914ed589010c3e2d6471e90729a
morgan thorne <morgangthorne@outlook.com>
2025-09-03 19:56:36 -0400

    Add project files.

---
+++ IDE_Quiz.cpp
@@ -0,0 +1,21 @@
+// IDE_Quiz.cpp : This file contains the 'main' function. Program execution begins and ends there.
+// This program will display my initials
+
+using namespace std;
+#include <iostream>
+
+
+int main()
+{
+    char first, middle, last;
+    
+    //Storing Initial Values
+    first = 'M';
+    middle = 'G';
+    last = 'T';
+
+    //Displays initials
+    cout << first << middle << last << endl;
+    
+    return 0;
+}
